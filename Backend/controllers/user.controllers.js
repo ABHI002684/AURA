9823a712c68c62972878002c3decd9deed92a8f2
ABHI002684 <abhishek002684@gmail.com>
2025-08-16 23:35:32 +0530

    created update asistant api endpoint

--- Backend/controllers/user.controllers.js
+++ Backend/controllers/user.controllers.js
@@ -1,4 +1,5 @@
 import User from '../models/user.model.js';
+import uploadOnCloudinary from '../config/cloudinary.js';
 
 export const getCurrentUser = async (req,res)=>{
     try{
@@ -12,5 +13,26 @@ export const getCurrentUser = async (req,res)=>{
         return res.status(200).json(user);
     }catch(err){
         return res.status(500).json("error in getting current user");
+    }
+}
+
+export const updateAssistant =async(req,res)=>{
+    try{
+        const {assistantName,imageUrl}=req.body;
+        let avtar;
+
+        if(req.file){
+            avtar= await uploadOnCloudinary(req.file.path);
+        }else{
+            avtar=imageUrl;
+        }
+
+        const user= await User.findByIdAndUpdate(req.userId,{
+            assistantName,avtar
+        },{new:true}).select("-password");
+
+        return res.status(200).json("assistant updated successfully",user);
+    }catch(err){
+        return res.status(500).json("error in updating assistant");
     }
 }
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { getCurrentUser } from '../controllers/user.controllers.js';
+import { getCurrentUser,updateAssistant } from '../controllers/user.controllers.js';
 import isAuth from '../middlewares/isAuth.js';
+import upload from '../middlewares/multer.js';
 const router=express.Router();
 
 router.get('/current',isAuth,getCurrentUser);
-
+router.post('/update',isAuth,upload.single('avtar'),updateAssistant);
 export default router;
